3b61528db6189d9cc665032f03b173dda2f7d4a6
Mariano <marianonunezma@gmail.com>
2022-06-05 21:51:48 -0300

    Header Ready

--- src/index.js
+++ src/index.js
@@ -10,23 +10,51 @@ navbar.className="navbar";
 
 //Logo
 const logoContainer = document.createElement("div");
-logoContainer.className="logo-container";
+logoContainer.className = "logo-container";
 
 const logoImg = document.createElement("img");
-logoImg.src=logoImage;
+logoImg.src = logoImage;
 
 logoContainer.appendChild(logoImg);
 
-//Tags
+//Btn
 const btnContainer = document.createElement("div");
-btnContainer.className="btn-container";
+btnContainer.className = "btn-container";
 
 const btnHome = document.createElement("a");
+btnHome.className = "btn";
+btnHome.textContent = "Home";
 
+const btnMenu = document.createElement("a");
+btnMenu.className = "btn";
+btnMenu.textContent = "Menu";
+
+const btnAbout = document.createElement("a");
+btnAbout.className = "btn";
+btnAbout.textContent = "About";
+
+btnContainer.appendChild(btnHome);
+btnContainer.appendChild(btnMenu);
+btnContainer.appendChild(btnAbout);
+
+btnHome.addEventListener('click', () => {
+    console.log("btnHome");
+    btnHome.className = "btn active";
+});
+
+btnMenu.addEventListener('click', () => {
+    console.log("btnMenu");
+});
+
+btnAbout.addEventListener('click', () => {
+    console.log("btnAbout");
+});
 
 navbar.appendChild(logoContainer);
 navbar.appendChild(btnContainer);
 
 content.appendChild(navbar);
+
+
 
 
